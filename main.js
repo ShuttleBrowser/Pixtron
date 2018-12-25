374c28490c23577ceecf1a329cc200ce0106ffc0
Robin Jullian <38robin38@gmail.com>
2018-12-25 13:16:11 +0100

    add config file + add downloading from github

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
 const fs = require('fs')
+const request = require('request')
+const fetch = require('node-fetch')
 
 const express = require('express')
 const app = express()
+
+const config = require('./config.json')
+
+/*
+  console.log(`${releasesURL}/${config.releases[x].files[y]}`)
+  console.log(`${__dirname}/releases/latest/${config.releases[x].platform}/${config.releases[x].files[y]}`)
+  console.log(`${releasesURL}/${config.releases[x].files[y]}`)
+*/
+
+const downloadFromGithub = (releasesURL) => {
+  
+  let url
+  let options
+
+  for (x in config.releases) {
+    console.log(`===== ${config.releases[x].platform } =====`)
+    for (y in config.releases[x].files) {
+
+      url = `${releasesURL}/${config.releases[x].files[y]}`
+
+      options = {
+        directory: `${__dirname}/releases/latest/${config.releases[x].platform}`,
+        filename: config.releases[x].files[y]
+      }
+
+      console.log(`Downloading ${options.filename}...`)
+      request(url).pipe(fs.createWriteStream(`${options.directory}/${options.filename}`))
+    }
+  }
+
+}
 
 app.get('/update/:version/:platform/:file', (req, res) => {
   res.sendFile(`${__dirname}/releases/${req.params.version}/${req.params.platform}/${req.params.file}`)
@@ -28,6 +61,38 @@ app.get('/update/', (req, res) => {
   })
 })
 
+app.get('/upload/:tag/:password', (req, res) => {
+  const releasesURL = `https://github.com/${config.githubRepo}/releases/download/${req.params.tag}`
+  const gituhApi = `https://api.github.com/repos/${config.githubRepo}/tags`
+
+  if (req.params.password === config.password) {
+    fetch(gituhApi)
+    .then(resp => resp.json())
+    .then(data => {
+      for (i in data) {
+
+        if (data[i].name === req.params.tag) {
+          res.json({
+            message: "DOWNLOADING"
+          })
+
+          downloadFromGithub(releasesURL)
+          break
+        }
+
+      }
+    }).catch((e) => {
+      res.json({
+        message: e
+      })
+    })
+  } else {
+    res.json({
+      message: "BAD_PASSWORD"
+    })
+  }
+})
+
 app.get('*', (req, res) => {
   res.json({
     welcome: 'Welcome on pixtron, a (very) simple update server for electron-updater',
@@ -37,5 +102,7 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 1004;
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
+    console.log(`[INFO] > http://localhost:${port}`)
 })
+
+// https://github.com/ShuttleBrowser/Shuttle/releases/download/3.0.0-beta-01/latest-linux.yml
